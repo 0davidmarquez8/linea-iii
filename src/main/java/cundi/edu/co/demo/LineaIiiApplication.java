9064e7cf950240e2dc01d3ab270a79e18cf00241
0davidmarquez8 <davidmarquezing@gmail.com>
2021-11-29 19:10:28 -0500

    Update LineaIiiApplication.java

--- src/main/java/cundi/edu/co/demo/LineaIiiApplication.java
+++ src/main/java/cundi/edu/co/demo/LineaIiiApplication.java
@@ -2,13 +2,20 @@ package cundi.edu.co.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LineaIiiApplication {
+public class LineaIiiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(LineaIiiApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(LineaIiiApplication.class); // Replace DemoApplication with your main class
+    }
 
 }
